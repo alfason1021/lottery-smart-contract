6ecd27e4e7653cc3f13aa05a48aaa37e5048e5f9
alfason1021 <65756839+alfason1021@users.noreply.github.com>
2022-02-28 10:46:39 -0600

    Update lottery-samrt-contract.sol

--- lottery-samrt-contract.sol
+++ lottery-samrt-contract.sol
@@ -1,47 +1,68 @@
-// SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: GPL-3.0
  
 pragma solidity >=0.5.0 <0.9.0;
 
-contract Lottery {
-    // declaring state variables
-    address payable[] public players; // dyna,ic array of type address payable.
-    address public manager;
-
-    constructor() {
-        // initializing the manager that deploys the contract
-        manager = msg.sender;
+contract Lottery{
+    
+    // declaring the state variables
+    address payable[] public players; //dynamic array of type address payable
+    address public manager; 
+    
+    
+    // declaring the constructor
+    constructor(){
+        // initializing the owner to the address that deploys the contract
+        manager = msg.sender; 
+        
     }
+    
+    // declaring the receive() function that is necessary to receive ETH
+    receive () payable external{
 
-    // the receive function ensures the contract receives eth
-    receive() external payable {
+        // each player sends exactly 0.1 ETH 
         require(msg.value == 0.1 ether);
+        // appending the player to the players array
         players.push(payable(msg.sender));
     }
-
-    // to return the contract balance only by the manager in wei
-    function getBalance() public view returns(uint) {
+    
+    // returning the contract's balance in wei
+    function getBalance() public view returns(uint){
+        // only the manager is allowed to call it
         require(msg.sender == manager);
         return address(this).balance;
     }
-
-    // helper function that returns a large random integer
-    function random() public view returns(uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    
+    // helper function that returns a big random integer
+    function random() internal view returns(uint){
+       return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
     }
-
-    function pickWinner() public {
-        require(msg.sender == manager); // for only manager to pick winner when players is more than 3
-        require(players.length >= 3);
-
+    
+    
+    // selecting the winner
+    function pickWinner() public{
+        // only the manager can pick a winner if there are at least 3 players in the lottery
+        require(msg.sender == manager);
+        require (players.length >= 3);
+        
         uint r = random();
         address payable winner;
-
-        uint index = r % players.length; // getting a random index of the array
-
-        winner  = players[index]; // winner
-
-        winner.transfer(getBalance()); // transferring the balance to the winner
-
-        players = new address payable[](0); // reset the lottery for next round.
+        
+        // computing a random index of the array
+        uint index = r % players.length;
+    
+        winner = players[index]; // this is the winner
+        
+        uint managerFee = (getBalance() * 10 ) / 100; // manager fee is 10%
+        uint winnerPrize = (getBalance() * 90 ) / 100;     // winner prize is 90%
+        
+        // transferring 90% of contract's balance to the winner
+        winner.transfer(winnerPrize);
+        
+        // transferring 10% of contract's balance to the manager
+        payable(manager).transfer(managerFee);
+        
+        // resetting the lottery for the next round
+        players = new address payable[](0);
     }
+
 }
